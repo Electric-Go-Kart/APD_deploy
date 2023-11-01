d3360291c9736c47b5d8608791e22d7c9a6d7433
cdchrisdog1 <123763180+cdchrisdog1@users.noreply.github.com>
2023-11-01 15:10:35 -0600

    Update run_it.sh

--- run_it.sh
+++ run_it.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/gokart/projects/APD_deploy/yolov5
-python detect.py --weights ./528Project/kittikaleov2-int8_320_edgetpu.tflite --img 320 --conf 0.25 --source 0 
-python detect1.py --weights ./528Project/kittikaleov2-int8_320_edgetpu.tflite --img 320 --conf 0.25 --source 2 
+python detect.py --weights ./528Project/kittikaleov2-int8_320_edgetpu.tflite --img 320 --conf 0.25 --source 0 &
+python detect1.py --weights ./528Project/kittikaleov2-int8_320_edgetpu.tflite --img 320 --conf 0.25 --source 2 &
+wait
+
